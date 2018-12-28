604248d1711ded678a04cfe2ad7093228d30a571
xb52 <xb52@cornell.edu>
2018-12-28 14:21:06 -0500

    Initial commit

---
+++ src/BrickBreaker/Main.java
@@ -0,0 +1,16 @@
+package BrickBreaker;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame obj = new JFrame();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Brick Breaker Game");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
