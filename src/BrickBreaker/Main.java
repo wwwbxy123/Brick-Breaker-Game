3468cd7bf4249357085c6614c514f33c5dcf00e0
xb52 <xb52@cornell.edu>
2018-12-28 23:13:09 -0500

    fix all bugs

--- src/BrickBreaker/Main.java
+++ src/BrickBreaker/Main.java
@@ -8,14 +8,18 @@ public class Main {
 
         JFrame obj = new JFrame();
         GamePlay gamePlay = new GamePlay();
+        
 
-        obj.setBounds(10,10,700,600);
+
+        obj.setBounds(10,10,700,580);
         obj.setTitle("Brick Breaker Game");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //obj.add(b);
         obj.add(gamePlay);
+
         obj.setVisible(true);
 
     }
